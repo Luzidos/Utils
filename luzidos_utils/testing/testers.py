521efdfa1792a07d016a9a82845d5077fa1f803a
Andres-Carranza <carranzamoreno9@gmail.com>
2024-05-18 20:38:55 -0700

    pushing testing utils

--- luzidos_utils/testing/testers.py
+++ luzidos_utils/testing/testers.py
@@ -9,7 +9,8 @@ from luzidos_utils.aws_io.s3 import read
 from luzidos_utils.aws_io.s3 import write
 
 class BaseTest(unittest.TestCase):
-    def __init__(self, test_configs_dir):
+    def __init__(self, function_to_test, test_configs_dir):
+        self.function_to_test = function_to_test
         self.test_configs_dir = test_configs_dir
     
     """
@@ -30,12 +31,12 @@ class BaseTest(unittest.TestCase):
     ******************************************************************
     """
 
-    def run_test_cases(self, function_to_test):
+    def tests_wrapper(self):
         for test_config_path in self.load_test_config_paths():
             test_name = test_config_path[len(self.test_configs_dir):]
             payload, mock_data, expected_data = self.load_test_data(test_config_path)
             with self.subTest(test_name):
-                self.run_test_case(function_to_test, payload, mock_data, expected_data)
+                self.run_test_case(payload, mock_data, expected_data)
             
 
     
@@ -52,12 +53,16 @@ class BaseTest(unittest.TestCase):
         return test_config_paths
         
     def load_test_data(self, test_config_path):
-        with open(test_config_path) as f:
-            test_data = json.load(f)
+        with open(test_config_path, 'r') as file:
+            file_contents = file.read()
+        test_configs = {}
+        exec(file_contents, {}, test_configs)
+        test_data = test_configs.get('test_configs')
         
         payload = test_data["payload"]
         mock_data = test_data["mock_data"]
         expected_data = test_data["expected_data"]
+        return payload, mock_data, expected_data
 
     """
     ******************************************************************
@@ -68,25 +73,25 @@ class BaseTest(unittest.TestCase):
     def patch_s3_read(self, mock_s3: MockS3):
         return patch.multiple(
             'luzidos_utils.aws_io.s3.read',
-            read_file_from_s3=self.mock_s3.mock_read_file_from_s3,
-            read_dir_filenames_from_s3=self.mock_s3.mock_read_dir_filenames_from_s3,
-            list_childdirectories=self.mock_s3.mock_list_childdirectories
+            read_file_from_s3=mock_s3.mock_read_file_from_s3,
+            read_dir_filenames_from_s3=mock_s3.mock_read_dir_filenames_from_s3,
+            list_childdirectories=mock_s3.mock_list_childdirectories
         )
     
     def patch_s3_write(self, mock_s3: MockS3):
         return patch.multiple(
             'luzidos_utils.aws_io.s3.write',
-            upload_file_to_s3=self.mock_s3.mock_upload_file_to_s3,
-            upload_fileobj_to_s3=self.mock_s3.mock_upload_fileobj_to_s3,
-            upload_dict_as_json_to_s3=self.mock_s3.mock_upload_dict_as_json_to_s3,
-            copy_file=self.mock_s3.mock_copy_file
+            upload_file_to_s3=mock_s3.mock_upload_file_to_s3,
+            upload_fileobj_to_s3=mock_s3.mock_upload_fileobj_to_s3,
+            upload_dict_as_json_to_s3=mock_s3.mock_upload_dict_as_json_to_s3,
+            copy_file=mock_s3.mock_copy_file
         )
 
 
 
 class ModuleTest(BaseTest):
-    def __init__(self, test_config_path):
-        super().__init__(test_config_path)
+    def __init__(self, function_to_test, test_configs_dir):
+        super().__init__(function_to_test, test_configs_dir)
     
     def setup(self):
         self.mock_s3 = None
@@ -96,7 +101,7 @@ class ModuleTest(BaseTest):
         self.expected_state = None
         self.module_name = None
 
-    def run_test_case(self, function_to_test, payload, mock_data, expected_data):
+    def run_test_case(self, payload, mock_data, expected_data):
         self.user_id = payload["user_id"]
         self.invoice_id = payload["invoice_id"]
         self.state_data = payload["state_data"]
@@ -107,7 +112,7 @@ class ModuleTest(BaseTest):
 
         with self.patch_s3_read(self.mock_s3), \
             self.patch_s3_write(self.mock_s3):
-            self.state_data = function_to_test(self.user_id, self.invoice_id, self.state_data)
+            self.state_data = self.function_to_test(self.user_id, self.invoice_id, self.state_data)
         
         self.assert_state_data()
 

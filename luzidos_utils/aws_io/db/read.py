14a721fe4b44cf07b309598efd82be32da5d850f
TiberioMalaiu <86580632+TiberioMalaiu@users.noreply.github.com>
2024-05-22 18:22:29 -0700

    naming again

--- luzidos_utils/aws_io/db/read.py
+++ luzidos_utils/aws_io/db/read.py
@@ -1,7 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
 
-def read_email_and_user_from_db(email):
+def get_user_from_db(email):
     # Initialize a session using Amazon DynamoDB
     dynamodb = boto3.resource('dynamodb')
 
@@ -27,4 +27,4 @@ def read_email_and_user_from_db(email):
 
 if __name__ == "__main__":
     # Test here.
-    # print(read_email_and_user_from_db('luzidos@luzidos.com'))
+    # print(get_user_from_db('luzidos@luzidos.com'))
